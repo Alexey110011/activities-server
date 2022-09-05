738ce5608dddf18e0e4f42f1811c771ee76d6372
Alexey <alleshkvch@gmail.com>
2022-09-05 11:34:18 +0300

    access

--- index.js
+++ index.js
@@ -7,11 +7,11 @@ app.listen(process.env.PORT||port, () => console.log(`Listening on port ${port}`
 
 app.use(express.json())
 
-app.use(function (req, res, next) {
+/*app.use(function (req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers');
-  next();});
+  next();});*/
 
 app.get('/m', (req, res) => {
   merchant_model.getContragents()
