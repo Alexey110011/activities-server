13f2788866886bce890a9ad4c8ed761bfc1e7ae4
Alexey <alleshkvch@gmail.com>
2022-09-05 17:19:45 +0300

    all

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ app.listen(process.env.PORT||port, () => console.log(`Listening on port ${port}`
 
 app.use(express.json())
 app.use(cors())
-app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type'/*, Access-Control-Allow-Headers'*/);
+app.all('/'/*use(*/,function (req, res, next) {
+  res.header/*setHeader*/('Access-Control-Allow-Origin', '*');
+  res.header/*setHeader*/('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+  res.header/*setHeader*/('Access-Control-Allow-Headers', 'Content-Type'/*, Access-Control-Allow-Headers'*/);
   next();});
 
 app.get('/m', (req, res) => {
