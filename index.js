3843eb0ad30dec21f37b9e71bc4e47897ede6996
Alexey <alleshkvch@gmail.com>
2022-09-06 21:30:44 +0300

    all

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.json())
 app.use(cors(
   {origin:'*'}
 ))
-app.all(function (req, res, next) {
+app.all('/',function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type', 'Access-Control-Allow-Headers');
