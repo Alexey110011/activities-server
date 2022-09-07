faaaa0c206688219ad35864a95a5d7b9fdc83a7a
Alexey <alleshkvch@gmail.com>
2022-09-07 10:52:35 +0300

    precise

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.json())
   headers: ['Content-Type','Access-Control-Allow-Headers']}
 ))*/
 app.all('/',function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', 'https://sparkling-malasada-6c08c8.netlify.app');
   res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type', 'Access-Control-Allow-Headers');
   next();});
