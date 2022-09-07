25795eb08f92afad39e1e803c4a3e0ab2b5df6d3
Alexey <alleshkvch@gmail.com>
2022-09-07 12:29:58 +0300

    options33

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.json())
 ))*/
 app.use(function (req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', 'https://sparkling-malasada-6c08c8.netlify.app');
-  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE')/*,OPTIONS')*/;
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type', 'Access-Control-Allow-Headers');
   next();});
 
