4e34d59748b83560bff92473e4644335a709029f
Alexey <alleshkvch@gmail.com>
2022-09-10 13:26:13 +0300

    reqbody

--- contragent_model.js
+++ contragent_model.js
@@ -21,6 +21,7 @@ const getContragents = () => {
 const createContragent = (body) => {
   return new Promise(function (resolve, reject) {
     const { _id, fullname, date, type, amount, email, phone, address, question } = body
+    console.log(body)
     pool.query('INSERT INTO contragents (_id, fullname, date, type, amount, email, phone, address, question) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING *', [_id, fullname, date, type, amount, email, phone, address, question], (error, results) => {
       if (error) {
         reject(error)
