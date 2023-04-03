f9d5dc38324f5dd016dc03e2125cfc6a902f0f4d
Alexey <alleshkvch@gmail.com>
2023-04-03 11:21:28 +0300

    c

--- model.js
+++ model.js
@@ -11,7 +11,7 @@ const pool = new Pool({
 const getAllBooks = ()=>{
     return new Promise(function(resolve, reject){
         pool.query(
-            `SELECT * FROM contragents ORDER BY _id ASC`, (error, results)=>{
+            `SELECT * FROM book ORDER BY authors ASC`, (error, results)=>{
                 if(error) {reject(error)};
                 resolve(results.rows)
             })
