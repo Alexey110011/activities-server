62d752b2e3e96a7d7badd58ce563fd87d9cdc85c
Alexey <alleshkvch@gmail.com>
2023-04-12 19:16:23 +0300

    1204

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.use(function (req, res, next) {
   if(allowedOrigin.includes(origin)){
     res.setHeader('Access-Control-Allow-Origin', origin)
   }*/
-  res.setHeader("Access-Control-Allow-Origin", "*")
+  res.setHeader('Access-Control-Allow-Origin', '*')
   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type', 'Access-Control-Allow-Headers','Authorization');
   next();});
